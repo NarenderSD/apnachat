037b3cce3d5f3f23486b52ec7f246a270a7212b5
Narender Singh <109752675+NarenderSD@users.noreply.github.com>
2024-03-06 00:18:45 +0530

    Chat App

--- bchat/src/App.js
+++ bchat/src/App.js
@@ -1,11 +1,19 @@
-
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Join from './component/Join/Join';
+import Chat from './component/Chat/Chat';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Chat App Working</h1>
-    </div>
+    <Router>
+      {/* Wrap your routes with the <Routes> component */}
+      <Routes>
+        {/* Each route is wrapped by a <Route> component */}
+        <Route path="/" element={<Join />} />
+        <Route path="/chat" element={<Chat />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
   );
 }
 
